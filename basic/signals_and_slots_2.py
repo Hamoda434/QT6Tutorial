26ae7c6ef3a96ac0e3c0974f1a81c5debd08f63b
Hayley Davis <hayley@hayley-dell-laptop.hq.laserthermal.com>
2025-09-16 15:48:08 -0400

    Completed Chapter 1-5

---
+++ basic/signals_and_slots_2.py
@@ -0,0 +1,34 @@
+import sys
+
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QPushButton,
+)
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.button_is_checked = True
+
+        self.setWindowTitle("My App")
+
+        self.button = QPushButton("Press Me!")
+        self.button.clicked.connect(self.the_button_was_clicked)
+
+        self.setCentralWidget(self.button)
+
+    def the_button_was_clicked(self):
+        self.button.setText("You already clicked me.")
+        self.button.setEnabled(False)
+
+        self.setWindowTitle("My Oneshot App")
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
