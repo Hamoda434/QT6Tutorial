7c24eea10591e2c2b716cc4c5049f756a863a8d1
Hayley Davis <hayley@hayley-dell-laptop.hq.laserthermal.com>
2025-09-18 11:24:58 -0400

    Completed up to Chapter 15

---
+++ designer/qresource.py
@@ -0,0 +1,23 @@
+import sys
+
+from PySide6 import QtGui, QtWidgets
+
+import resources
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Hello World")
+        b = QtWidgets.QPushButton("My button")
+
+        icon = QtGui.QIcon(":/icons/land.jpg")
+        b.setIcon(icon)
+        self.setCentralWidget(b)
+
+        self.show()
+
+app = QtWidgets.QApplication(sys.argv)
+w = MainWindow()
+app.exec()
+
